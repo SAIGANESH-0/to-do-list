76416d1cbcb26a364f0af37d2a61c29c91142c4d
saiganeshvoona <saiganeshvoona4@gmail.com>
2023-03-11 12:44:44 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TO DO LIST APP</h1>
+      <TodoWrapper  />
+    </div>
+  );
+}
+
+export default App;
